9e9dfc8eb9a5534acac96e06330ba2915cae2c93
andrew <andrewsamagachandra@gmail.com>
2024-06-07 10:29:16 +0700

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,25 @@
+// Animation scroll
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+// Toggle & Responsive Navigation
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+
+navSlide();
